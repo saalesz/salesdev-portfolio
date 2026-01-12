c551c56d4a0e039f7a367405f9c4a03a3c57e781
Emerson Sales <emersonsantossales245@gmail.com>
2026-01-12 18:05:40 -0300

    Exclusão das pastas e arquivos relacionados a Links Page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./Pages";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* Rota do Portfólio Principal (Dark) */}
-        <Route path="/" element={<Index />} />
-
-        {/* Rota da Links Page (Light) - O '*' permite sub-rotas dentro dela 
-        <Route path="/links/*" element={<LinksPage />} />
-        */}
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Index />;
 }
 
 export default App;
